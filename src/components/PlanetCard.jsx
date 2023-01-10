8d34a9d15920f1eb96ac46d0918663c27052983d
Logistis <leandrofigueiredofranco@gmail.com.br>
2023-01-10 15:16:50 -0300

    req 5

---
+++ src/components/PlanetCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName } = this.props;
+    const { PlanetImage } = this.props;
+    return (
+      <div data-testid="planet-card">
+        <p data-testid="planet=name">{planetName}</p>
+        <img src={ PlanetImage } alt={ `planeta ${planetName}` } />
+      </div>
+    );
+  }
+}
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  PlanetImage: PropTypes.string.isRequired,
+};
+
+export default PlanetCard;
