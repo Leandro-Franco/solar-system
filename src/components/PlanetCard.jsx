8843e71454215baef820f5f99e6f6a117fd4ded4
Logistis <leandrofigueiredofranco@gmail.com.br>
2023-01-10 15:54:41 -0300

    ajuste na 5

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -4,18 +4,18 @@ import PropTypes from 'prop-types';
 class PlanetCard extends React.Component {
   render() {
     const { planetName } = this.props;
-    const { PlanetImage } = this.props;
+    const { planetImage } = this.props;
     return (
       <div data-testid="planet-card">
         <p data-testid="planet-name">{planetName}</p>
-        <img src={ PlanetImage } alt={ `planeta ${planetName}` } />
+        <img src={ planetImage } alt={ `planeta ${planetName}` } />
       </div>
     );
   }
 }
 PlanetCard.propTypes = {
   planetName: PropTypes.string.isRequired,
-  PlanetImage: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
 };
 
 export default PlanetCard;
