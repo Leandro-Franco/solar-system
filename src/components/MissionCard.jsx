28e4df6c38e88f7ec541ad5982bf22dd691e3a7d
Logistis <leandrofigueiredofranco@gmail.com.br>
2023-01-10 17:52:24 -0300

    req 5

---
+++ src/components/MissionCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class MissionCard extends React.Component {
+  render() {
+    return (
+      <div data-testid="mission-card" />
+    );
+  }
+}
+
+export default MissionCard;
